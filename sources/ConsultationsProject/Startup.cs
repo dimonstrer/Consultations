f9d19c0db2e44f6b9be3f83d0af14158e1f918dc
Dmitry <dimon@hu2.ru>
2020-03-24 17:25:14 +0500

    fix(back): Отменена автоматическая отправка ответов api контроллеров при непройденной валидации модели #8

--- sources/ConsultationsProject/Startup.cs
+++ sources/ConsultationsProject/Startup.cs
@@ -6,6 +6,7 @@ using ConsultationsProject.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,9 @@ namespace ConsultationsProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApiBehaviorOptions>(apiBehaviorOptions => {
+                apiBehaviorOptions.SuppressModelStateInvalidFilter = true;
+            });
             services.AddControllersWithViews();
             services.AddDbContext<PatientsContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("PatientsDbConnectionString")));
