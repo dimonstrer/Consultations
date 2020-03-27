60fbd504280a21c2d5a91b70f89807b96ac9849c
Dmitry <dimon@hu2.ru>
2020-03-27 15:54:38 +0500

    feat(back): Добавлены DTO модели пациента и консультации #11

---
+++ sources/ConsultationsProject/Models/DTO/ConsultationDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsultationsProject.Models.DTO
+{
+    /// <summary>
+    /// Модель передачи данных консультации пациента.
+    /// </summary>
+    public class ConsultationDTO
+    {
+        /// <summary>
+        /// День консультации пациента.
+        /// </summary>
+        [Display(Name = "Дата*")]
+        [Required(ErrorMessage = "Не указана дата консультации")]
+        public DateTime Day { get; set; }
+
+        /// <summary>
+        /// Время консультации пациента.
+        /// </summary>
+        [Display(Name = "Время*")]
+        [Required(ErrorMessage = "Не указано время консультации")]
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Симптомы пациента, записанные на консультации.
+        /// </summary>
+        [Display(Name = "Симптомы")]
+        public string Symptoms { get; set; }
+    }
+}
---
+++ sources/ConsultationsProject/Models/DTO/PatientDTO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsultationsProject.Models.DTO
+{
+    /// <summary>
+    /// Модель передачи данных пациента.
+    /// </summary>
+    public class PatientDTO
+    {
+        /// <summary>
+        /// Имя пациента.
+        /// </summary>
+        [Display(Name = "Имя*")]
+        [Required(ErrorMessage = "Не указано имя пациента")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия пациента.
+        /// </summary>
+        [Display(Name = "Фамилия*")]
+        [Required(ErrorMessage = "Не указана фамилия пациента")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Отчество пациента.
+        /// </summary>
+        [Display(Name = "Отчество (если присутствует)")]
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// Дата рождения пациента.
+        /// </summary>
+        [Display(Name = "Дата рождения*")]
+        [Required(ErrorMessage = "Не указана дата рождения пациента")]
+        public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// Пол пациента.
+        /// </summary>
+        [Display(Name = "Пол*")]
+        [Required(ErrorMessage = "Не указан пол пациента")]
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// СНИЛС пациента.
+        /// </summary>
+        [Display(Name = "СНИЛС*")]
+        [Required(ErrorMessage = "Не указан СНИЛС пациента")]
+        public string PensionNumber { get; set; }
+    }
+}
